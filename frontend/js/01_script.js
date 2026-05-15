29a073327720a851d480e542fac0aba1cc54b6fa
Shivam Pawar <shivampawar1218@gmail.com>
2026-05-16 01:56:39 +0530

    Javascript File Added

---
+++ frontend/js/01_script.js
@@ -0,0 +1,21 @@
+function openSidebar() {
+
+            document
+                .getElementById("sidebar")
+                .classList.add("active");
+
+            document
+                .getElementById("overlay")
+                .classList.add("active");
+        }
+
+        function closeSidebar() {
+
+            document
+                .getElementById("sidebar")
+                .classList.remove("active");
+
+            document
+                .getElementById("overlay")
+                .classList.remove("active");
+        }
